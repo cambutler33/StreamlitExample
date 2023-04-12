9b756fe4a5dea7d534fa1c14aec44b4e29c73a65
Cameron Butler <cameronsbutler33@gmail.com>
2023-04-11 19:27:45 -0600

    done

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import plotly.express as px
 import re
 
 st.write("""
-## Bayes Rule Application: 
-### P(Year of Birth | Name and Sex)
+## Birth Year
 """)
 
 usa = pd.read_csv('names_usa.csv')
@@ -59,32 +58,16 @@ with st.expander('See comparison of prior'):
     plt.legend(loc=(1.01,0))
     st.pyplot(fig1)
 
-pattern = st.text_input("Type a name to search", placeholder="Enter a name or regular expression")
-
-regex = st.radio(
-    "Is your search a regular expression?",
-    ('No','Yes'))
+pattern = st.text_input("Type a name to search", placeholder="Enter a name")
     
-cond_sex = st.radio("Do you want to condition on a sex?",
-   ("No", "Yes"))
-    
-if cond_sex == "Yes":
-        sex = st.radio("Which sex?",
-            ("M", "F"))
-        sex = set(sex)
-    
-else:
-        sex = set(["M", "F"])
+sex = set(["M", "F"])
 
 if len(pattern) > 0:
     df = usa[usa.sex.isin(sex)]
     df = df.merge(df.groupby('year').sum().reset_index(), on='year')
     df['pname'] = df['n_x']/df['n_y']
     unique_names = set(df['name'])
-    if regex=='Yes':
-        keep = {name for name in unique_names if re.match(pattern, name)}
-    else:
-        keep = {pattern}
+    keep = {pattern}
     nameDF = df[df['name'].isin(keep)]
     keep_names = pd.DataFrame(list(keep), columns=['Names']).sort_values(by='Names')
     num = nameDF.groupby('year')['pname'].sum()
